0ebc7c82283bfcffa06f5bc1ac4da84ac7d22201
acaternberg <acaternberg@cloudbees.com>
2026-06-23 17:41:37 +0200

    fix section

--- gke/envoy/install.sh
+++ gke/envoy/install.sh
@@ -139,7 +139,7 @@ helm upgrade --install cloudbees-core-envoy cloudbees/cloudbees-core \
   --namespace "${NAMESPACE}" \
   --set Gateway.Enabled=true \
   --set Gateway.Name="${GATEWAY_NAME}" \
-  --set Gateway.SectionName=http \
+  --set Gateway.SectionName=https \
   --set Gateway.Namespace="${NAMESPACE}" \
   --set OperationsCenter.HostName="${CJOC_HOST_NAME}" \
   --set OperationsCenter.Protocol=https \
